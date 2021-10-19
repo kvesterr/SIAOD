f185ef6e7d0475dd2405e199c0fd0df985cd3471
kvesterr <56050759+kvesterr@users.noreply.github.com>
2021-10-19 16:16:12 +0300

    Update stack.py

--- SIAOD_4/stack.py
+++ SIAOD_4/stack.py
@@ -1,18 +1,23 @@
 class stack:
+    """Класс реализующий структуру данных Стек"""
 
     def __init__(self, arg):
+        """Конструктор класса Стек"""
         self.stack = list(arg)
 
     def is_empty(self):
+        """Метод проверяющий содержит ли Стек элементы"""
         if len(self.stack) == 0:
             return True
         else:
             return False
 
     def add(self, obj):
+        """Метод добавления элемента в Стек"""
         self.stack.insert(0,obj)
 
     def remove(self):
+        """Метод удаления элемента из Стека"""
         if len(self.stack) == 0:
             return -1
         else:
@@ -21,6 +26,7 @@ class stack:
             return temp
 
     def look(self):
+        """Метод возвращающий последний элемент занесенный в Стек, при этом сам элемент из Стека не удаляется"""
         temp = self.remove()
         if not (temp == -1):
             self.add(temp)
