f466490dafe7fd69104947ff00de14d262094bc8
kvesterr <56050759+kvesterr@users.noreply.github.com>
2021-10-19 16:09:05 +0300

    Update task1.py

--- SIAOD_TASKS/parliament/task1.py
+++ SIAOD_TASKS/parliament/task1.py
@@ -1,14 +1,17 @@
 def check(file):
+    """ Функция проверяющий парламентскую сессию на корректное внесение и закрытие проектов """
+
     f = open(file, 'r').readlines()
     count = int(f[0])
     stack = []
 
     for i in range(1, count + 1):
-        if f[i][0] == 'A':# Добавляем проект
-            stack.append(str(f[i][3]))
-        else: # Закрываем проект
+        if f[i][0] == 'A':  # Добавляем проект
+            stack.append(str(f[i][3]))  # f[i][3] - название партии внесшей проект
+        else:  # Закрываем проект
             if len(stack) > 0:
-                if stack[len(stack) - 1] == str(f[i][4]):
+                if stack[len(stack) - 1] == str(f[i][4]):  # Если сейчас закрывается последний внесенный проект
+                    # f[i][4] - название партии закрывающей проект
                     stack.pop()
             else:
                 return 'No'
