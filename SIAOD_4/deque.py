93f0aaabcc9fe41d9b3044b658306ee4f15f672d
kvesterr <56050759+kvesterr@users.noreply.github.com>
2021-10-19 16:15:35 +0300

    Update deque.py

--- SIAOD_4/deque.py
+++ SIAOD_4/deque.py
@@ -1,21 +1,27 @@
 class deque:
+    """Класс реализующий структуру данных Дек"""
 
     def __init__(self, arg):
+        """Конструктор класса Дек"""
         self.deque = list(arg)
 
     def __str__(self):
+        """Метод возвращающий Дек в виде строки"""
         return str(self.deque)
 
     def is_empty(self):
+        """Метод проверяющий содержит ли Дек элементы"""
         if len(self.deque) == 0:
             return True
         else:
             return False
 
     def add_start(self, obj):
+        """Метод добавления элемента в начало Дека"""
         self.deque.insert(0,obj)
 
     def remove_start(self):
+        """Метод удаления элемента из начала Дека"""
         if self.is_empty():
             return -1
         temp = self.deque[0]
@@ -23,7 +29,9 @@ class deque:
         return temp
 
     def add_end(self, obj):
+        """Метод добавления элемента в конец Дека"""
         self.deque.append(obj)
 
     def remove_end(self):
+        """Метод удаления элемента с конца дека"""
         return self.deque.pop()
